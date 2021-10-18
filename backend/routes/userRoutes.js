9770bfcf72961a3bbca6959eee0f395a178f25b6
mazeneleabd <mazenelabd98@gmail.com>
2021-10-18 11:12:40 +0200

    backend: admin-feature: delete & get & update user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,17 +4,28 @@ import { protect, admin } from '../middleware/authMiddleware.js'
 
 import {
   authUser,
+  deleteUser,
+  getUserById,
   getUserProfile,
   getUsers,
   registerUser,
+  updateUser,
   updateUserProfile,
 } from '../controllers/userController.js'
 
 router.post('/login', authUser)
+
 router.route('/').post(registerUser).get(protect, admin, getUsers)
+
 router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile)
+
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
 export default router
