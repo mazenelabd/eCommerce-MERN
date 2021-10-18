4c77d1020d57138e28a11bf50fd35b24a5e33727
mazeneleabd <mazenelabd98@gmail.com>
2021-10-18 10:02:40 +0200

    backend: feature: Register a new user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
 const router = express.Router()
-import { authUser } from '../controllers/userController.js'
+import { authUser, registerUser } from '../controllers/userController.js'
 
 router.post('/login', authUser)
+router.route('/').post(registerUser)
 
 export default router
