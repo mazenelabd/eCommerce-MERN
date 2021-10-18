46caefa550e33167a8e57b86ed16f6e27baafebd
mazeneleabd <mazenelabd98@gmail.com>
2021-10-18 10:31:20 +0200

    backend: admin-feature: get all users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,17 @@
 import express from 'express'
 const router = express.Router()
-import { protect } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 import {
   authUser,
   getUserProfile,
+  getUsers,
   registerUser,
   updateUserProfile,
 } from '../controllers/userController.js'
 
 router.post('/login', authUser)
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router
   .route('/profile')
   .get(protect, getUserProfile)
