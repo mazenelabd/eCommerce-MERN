25b3c1cdf8b0ef503e8550271b7376ecfad6a6da
mazeneleabd <mazenelabd98@gmail.com>
2021-10-18 16:06:20 +0200

    backend: feature: Fetch single product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,4 +25,18 @@ const getProducts = asyncHandler(async (req, res) => {
   res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
 
-export { getProducts }
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export { getProducts, getProductById }
