95e34117839eb8793189f983d4bc65280f1bb4c6
mazeneleabd <mazenelabd98@gmail.com>
2021-10-18 10:24:26 +0200

    backend: feature: get & update profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,19 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, registerUser } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
+
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from '../controllers/userController.js'
 
 router.post('/login', authUser)
 router.route('/').post(registerUser)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 export default router
