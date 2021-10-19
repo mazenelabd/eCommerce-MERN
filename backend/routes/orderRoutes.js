7468198465c07e9133c40c9bf09ae4c3c87efee6
mazeneleabd <mazenelabd98@gmail.com>
2021-10-19 06:07:56 +0200

    backend: feature: update order (paid & delivered)

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,11 +4,15 @@ import {
   addOrderItems,
   getMyOrders,
   getOrderById,
+  updateOrderToDelivered,
+  updateOrderToPaid,
 } from '../controllers/orderController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { admin, protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 
 export default router
