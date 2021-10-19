8302f6ad75ac734480a6e630c915db30489a97e8
mazeneleabd <mazenelabd98@gmail.com>
2021-10-19 06:09:22 +0200

    backend: feature: get all orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -106,10 +106,19 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Get all orders
+// @route   GET /api/orders
+// @access  Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate('user', 'id name')
+  res.json(orders)
+})
+
 export {
   addOrderItems,
   getMyOrders,
   getOrderById,
   updateOrderToPaid,
   updateOrderToDelivered,
+  getOrders,
 }
