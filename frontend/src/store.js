cb78d4c1f9cea547a0945863ee107c45d6e9b7b9
mazeneleabd <mazenelabd98@gmail.com>
2021-10-22 09:04:27 +0200

    frontend: redux store setup

---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(
+  reducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
