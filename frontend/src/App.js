b91d9c6fcb0bb8619a5fbd72e70cd5a3a3274ce3
mazenelabd <--global>
2022-07-29 19:30:23 +0200

    (update): the router to use lazy loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,73 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import Login from './screens/Login'
-import Register from './screens/Register'
-import Profile from './screens/Profile'
+import React, { lazy, Suspense } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Loader from './components/Loader'
 import Header from './components/Header'
-import CreateProduct from './screens/CreateProduct'
-import Home from './screens/Home'
-import Product from './screens/Product'
-import Cart from './screens/Cart'
-import Shipping from './screens/Shipping'
-import PlaceOrder from './screens/PlaceOrder'
-import Order from './screens/Order'
-import MyOrders from './screens/MyOrders'
-import OrderList from './screens/OrderList'
-import UserList from './screens/UserList'
-import UserEdit from './screens/UserEdit'
-import ProductList from './screens/ProductList'
-import ProductEdit from './screens/ProductEdit'
-import CategoryList from './screens/CategoryList'
-import CreateCategory from './screens/CreateCategory'
-import CategoryEdit from './screens/CategoryEdit'
 import Footer from './components/Footer'
+const Login = lazy(() => import('./screens/Login'))
+const Register = lazy(() => import('./screens/Register'))
+const Profile = lazy(() => import('./screens/Profile'))
+const CreateProduct = lazy(() => import('./screens/CreateProduct'))
+const Home = lazy(() => import('./screens/Home'))
+const Product = lazy(() => import('./screens/Product'))
+const Cart = lazy(() => import('./screens/Cart'))
+const Shipping = lazy(() => import('./screens/Shipping'))
+const PlaceOrder = lazy(() => import('./screens/PlaceOrder'))
+const Order = lazy(() => import('./screens/Order'))
+const MyOrders = lazy(() => import('./screens/MyOrders'))
+const OrderList = lazy(() => import('./screens/OrderList'))
+const UserList = lazy(() => import('./screens/UserList'))
+const UserEdit = lazy(() => import('./screens/UserEdit'))
+const ProductList = lazy(() => import('./screens/ProductList'))
+const ProductEdit = lazy(() => import('./screens/ProductEdit'))
+const CategoryList = lazy(() => import('./screens/CategoryList'))
+const CreateCategory = lazy(() => import('./screens/CreateCategory'))
+const CategoryEdit = lazy(() => import('./screens/CategoryEdit'))
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <main style={{ minHeight: '81.6vh' }}>
-        <Route path='/order/:id' component={Order} />
-        <Route path='/myorders' component={MyOrders} />
-        <Route path='/shipping' component={Shipping} />
-        <Route path='/placeorder' component={PlaceOrder} />
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
-        <Route path='/profile' component={Profile} />
-        <Route path='/admin/orderlist' component={OrderList} />
-        <Route path='/admin/categorylist' component={CategoryList} />
-        <Route path='/admin/userlist' component={UserList} />
-        <Route path='/admin/user/:id/edit' component={UserEdit} />
-        <Route path='/admin/product/create' component={CreateProduct} />
-        <Route path='/admin/category/create' component={CreateCategory} />
-        <Route path='/product/:id' component={Product} />
-        <Route path='/admin/productlist' component={ProductList} exact />
-        <Route
-          path='/admin/productlist/:pageNumber'
-          component={ProductList}
-          exact
-        />
-        <Route path='/admin/product/:id/edit' component={ProductEdit} />
-        <Route path='/admin/category/:id/edit' component={CategoryEdit} />
+    <Suspense fallback={<Loader />} >
+      <BrowserRouter>
+        <Header />
+        <main style={{ minHeight: '81.6vh' }}>
+          <Switch >
+            <Route path='/order/:id' component={Order} />
+            <Route path='/myorders' component={MyOrders} />
+            <Route path='/shipping' component={Shipping} />
+            <Route path='/placeorder' component={PlaceOrder} />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+            <Route path='/profile' component={Profile} />
+            <Route path='/admin/orderlist' component={OrderList} />
+            <Route path='/admin/categorylist' component={CategoryList} />
+            <Route path='/admin/userlist' component={UserList} />
+            <Route path='/admin/user/:id/edit' component={UserEdit} />
+            <Route path='/admin/product/create' component={CreateProduct} />
+            <Route path='/admin/category/create' component={CreateCategory} />
+            <Route path='/product/:id' component={Product} />
+            <Route path='/admin/productlist' component={ProductList} exact />
+            <Route
+              path='/admin/productlist/:pageNumber'
+              component={ProductList}
+              exact
+            />
+            <Route path='/admin/product/:id/edit' component={ProductEdit} />
+            <Route path='/admin/category/:id/edit' component={CategoryEdit} />
 
-        <Route path='/cart/:id?' component={Cart} />
-        <Route path='/search/:keyword' component={Home} exact />
-        <Route path='/page/:pageNumber' component={Home} exact />
-        <Route
-          path='/search/:keyword/page/:pageNumber'
-          component={Home}
-          exact
-        />
-        <Route path='/category/:category' component={Home} exact />
-        <Route path='/' component={Home} exact />
-      </main>
-      <Footer />
-    </BrowserRouter>
+            <Route path='/cart/:id?' component={Cart} />
+            <Route path='/search/:keyword' component={Home} exact />
+            <Route path='/page/:pageNumber' component={Home} exact />
+            <Route
+              path='/search/:keyword/page/:pageNumber'
+              component={Home}
+              exact
+            />
+            <Route path='/category/:category' component={Home} exact />
+            <Route path='/' component={Home} exact />
+          </Switch>
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </Suspense>
   )
 }
 
